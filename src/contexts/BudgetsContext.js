c3b8b366ff7296783af0ed3887512d19afc44c43
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-02-08 17:07:42 +0200

    https://youtu.be/yz8x71BiGXg?t=1104

---
+++ src/contexts/BudgetsContext.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BudgetsContext = React.createContext();
+
+export function useBudgets() {}
+
+export const BudgetsProvider = ({ children }) => {
+  return children;
+};
